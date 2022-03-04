e2ce5d8cadbf82249b2d68e0cbfdafbb833e629a
Andrew Moore <andrew.moore9497@gmail.com>
2022-03-04 10:12:52 -0600

    part 14 done

--- youtube.cpp
+++ youtube.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <cmath>
+
+using std::cout;
+using std::cin;
+
+double power(double, int); //declaration
+void print_pow(double, int);
+
 int main() //main function
 {
-    std :: cout << "Hello World!\n";
-    return 0;
+    int base, exponent;
+    cout << "what is base?: ";
+    cin >> base;
+    cout << "what is exponent?: ";
+    cin >> exponent;
+
+    print_pow(base, exponent);
+}
+
+double power (double base, int exponent) //definition
+{
+    double result = 1;
+    for(int i = 0; i < exponent; i++)
+    {
+        result = result * base;
+    }
+    return result;
+}
+
+void print_pow(double base, int exponent)
+{
+    double myPower = power(base, exponent);
+    cout << base << " raised to the " << exponent << " power is " << myPower << std::endl;
 }
