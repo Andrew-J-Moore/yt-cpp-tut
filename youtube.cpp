260352ca0f50fa31c0e5ab8811b4ccb380551a7c
Andrew Moore <andrew.moore9497@gmail.com>
2022-03-04 12:59:34 -0600

    part 44 done

--- youtube.cpp
+++ youtube.cpp
@@ -1,35 +1,46 @@
 #include <iostream>
-#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 
-using std::cout;
-using std::cin;
-
-double power(double, int); //declaration
-void print_pow(double, int);
-
-int main() //main function
-{
-    int base, exponent;
-    cout << "what is base?: ";
-    cin >> base;
-    cout << "what is exponent?: ";
-    cin >> exponent;
-
-    print_pow(base, exponent);
-}
-
-double power (double base, int exponent) //definition
-{
-    double result = 1;
-    for(int i = 0; i < exponent; i++)
-    {
-        result = result * base;
+void play_game() {
+    int random = rand() % 251;
+    std::cout << random << std::endl;
+    std::cout << "Let's go!\n";
+    std::cout << "Guess a number: ";
+    while(true) {
+        int guess;
+        std::cin >> guess;
+        if(guess == random) {
+            std::cout << "You win!\n";
+            break;
+        } else if (guess < random) {
+            std::cout << "Too low!\n";
+        } else if (guess > random) {
+            std::cout << "Too high!\n";
+        }
     }
-    return result;
 }
 
-void print_pow(double base, int exponent)
+int main() 
 {
-    double myPower = power(base, exponent);
-    cout << base << " raised to the " << exponent << " power is " << myPower << std::endl;
+
+    srand(time(NULL));
+
+    int choice;
+    
+    do {
+        std::cout << "0. Quit\n1. Play Game\n";
+        std::cin >> choice;
+        switch(choice) {
+            case 0:
+                std::cout << "Thanks for nothing...\n";
+                break;
+            case 1:
+                play_game();
+                break;
+        }
+    }   
+    while(choice != 0);
+    
 }
