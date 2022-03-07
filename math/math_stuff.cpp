4eb6bce1b0bded600805aaf5797e8c3fea7c8d25
Andrew Moore <andrew.moore9497@gmail.com>
2022-03-07 12:31:20 -0600

    part 73 done

---
+++ math/math_stuff.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "math_utils.h"
+
+int main() {
+
+    std::cout << pow(3,3) << std::endl;
+    std::cout << pow(3) << std::endl; 
+
+    // Rectangle rectangle;
+    // rectangle.length = 10;
+    // rectangle.width = 5;
+
+    // std::cout << area(rectangle.length, rectangle.width) << std::endl;
+    // std::cout << area(rectangle.length) << std::endl;
+    // std::cout << area(rectangle) << std::endl;
+    return 0;
+}
